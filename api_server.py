ebfaed325821f909f9a02359d12cd7ec8fc8fc3d
HungryStudent <nikita.efan.2016@mail.ru>
2023-10-03 17:20:00 +0300

    fix api

--- api_server.py
+++ api_server.py
@@ -24,6 +24,7 @@ async def yookassa_request(request: Request):
 
 Ваш баланс: {user['balance']}₽""", reply_markup=user_kb.show_menu)
     # await check_wireguard_active(user_id, bot)
+
     devices = await db.get_devices_expired_sub_time_by_has_auto_renewal_and_user_id(True, user_id)
     for device in devices:
         user = await db.get_user(device["user_id"])
@@ -34,34 +35,24 @@ async def yookassa_request(request: Request):
         elif device["device_type"] == "outline":
             price = outline_prices[device["product_id"]]["price"]
             history_msg = "Оплата ключей"
-        devices = await db.get_devices_expired_sub_time_by_has_auto_renewal_and_user_id(True, user_id)
-        for device in devices:
-            user = await db.get_user(device["user_id"])
-            price = 0
-            history_msg = "Оплата конфигов"
+        if user["balance"] >= price:
+            await db.set_device_status(device["device_id"], True)
+            sub_time = datetime.now() + timedelta(days=31)
+            await db.set_sub_time(device["device_id"], sub_time)
+            server = await db.get_server(device["server_id"])
             if device["device_type"] == "wireguard":
-                price = wireguard_price
+                await server_utils.enable_wireguard_config(server["ip_address"], server["server_password"],
+                                                           device["device_id"])
             elif device["device_type"] == "outline":
-                price = outline_prices[device["product_id"]]["price"]
-                history_msg = "Оплата ключей"
-            if user["balance"] >= price:
-                await db.set_device_status(device["device_id"], True)
-                sub_time = datetime.now() + timedelta(days=31)
-                await db.set_sub_time(device["device_id"], sub_time)
-                server = await db.get_server(device["server_id"])
-                if device["device_type"] == "wireguard":
-                    await server_utils.enable_wireguard_config(server["ip_address"], server["server_password"],
-                                                               device["device_id"])
-                elif device["device_type"] == "outline":
-                    outline_manager = server_utils.Outline(server["outline_url"], server["outline_sha"])
-                    outline_client = outline_manager.get_client(device["outline_id"])
-                    outline_client_usage = outline_manager.get_usage_data(outline_client["id"]) // (1000 ** 3)
-                    await db.set_device_outline_traffic(device["device_id"], outline_client_usage)
-                    limit = outline_client['dataLimit']['bytes'] // (1000 ** 3) + outline_client_usage
-                    outline_manager.set_data_limit(device["outline_id"], limit)
+                outline_manager = server_utils.Outline(server["outline_url"], server["outline_sha"])
+                outline_client = outline_manager.get_client(device["outline_id"])
+                outline_client_usage = outline_manager.get_usage_data(outline_client["id"]) // (1000 ** 3)
+                await db.set_device_outline_traffic(device["device_id"], outline_client_usage)
+                limit = outline_client['dataLimit']['bytes'] // (1000 ** 3) + outline_client_usage
+                outline_manager.set_data_limit(device["outline_id"], limit)
 
-                await db.update_user_balance(user["user_id"], -price)
-                await db.add_history_record(user["user_id"], price, history_msg)
+            await db.update_user_balance(user["user_id"], -price)
+            await db.add_history_record(user["user_id"], price, history_msg)
 
     return 200
 
